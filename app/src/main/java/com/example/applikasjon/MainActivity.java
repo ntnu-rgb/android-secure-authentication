d28dca253ec3d7831616e484375ddaa5f6726de9
Linn-Mari Kristiansen <linn-mari.kristiansen@stud.ntnu.no>
2018-03-06 14:43:14 +0100

    Endret manifest filen til å gi riktig startskjerm

--- app/src/main/java/com/example/applikasjon/MainActivity.java
+++ app/src/main/java/com/example/applikasjon/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.applikasjon;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+   private String UuID = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,14 +19,13 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+  //IF UUID IKKE ER SATT, SEND TIL LOGG INN, ELLERS GJØR DET EIKA VIL VI SKAL GJØRE
+
+        if (UuID == null) {
+            Toast.makeText(this, "Viderefører til autentisering", Toast.LENGTH_SHORT).show();
+            this.startActivity(new Intent(this, FingerprintActivity.class));
+        }
+
     }
 
 }
