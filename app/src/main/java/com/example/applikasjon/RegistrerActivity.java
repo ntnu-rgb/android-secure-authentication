003c0c69a531dfee7467791a350803208469c84d
Linn-Mari <linn-mari.kristiansen@hig.no>
2018-03-08 21:59:08 +0100

    Registrering og innlogging

--- app/src/main/java/com/example/applikasjon/RegistrerActivity.java
+++ app/src/main/java/com/example/applikasjon/RegistrerActivity.java
@@ -1,9 +1,20 @@
 package com.example.applikasjon;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RegistrerActivity extends AppCompatActivity {
 
@@ -18,7 +29,41 @@ public class RegistrerActivity extends AppCompatActivity {
         final EditText pass = (EditText) findViewById(R.id.passord);
         final Button bRegistrer = (Button) findViewById(R.id.registrerOk);
 
+        bRegistrer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String bnavn = brukerNavn.getText().toString();
+                final String passord = pass.getText().toString();
+                Response.Listener<String> respons = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String respons) {
+                        Log.d("RESPONS", "UTHENTET"+respons);
 
+                        JSONObject jsonRespons = null;
+                        try {
+                            jsonRespons = new JSONObject(respons);
+                            boolean suksess = jsonRespons.getBoolean("suksess");
+
+                            if (suksess) {
+                               Intent intent = new Intent(RegistrerActivity.this, LogginnActivity.class);
+                                RegistrerActivity.this.startActivity(intent);
+
+                            }
+                            else {
+                                AlertDialog.Builder feil = new AlertDialog.Builder(RegistrerActivity.this);
+                                String feilmelding = jsonRespons.getString("Feilmelding");
+                                feil.setMessage("Feil ved innlogging").setNegativeButton(feilmelding, null).create().show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                RegistrerForesporsel regForesporsel = new RegistrerForesporsel(bnavn, passord, respons);
+                RequestQueue queue = Volley.newRequestQueue(RegistrerActivity.this);             //Legg inn forespørselen i køen for å kjøre den
+                queue.add(regForesporsel);
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/com/example/applikasjon/RegistrerForesporsel.java
@@ -0,0 +1,27 @@
+package com.example.applikasjon;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegistrerForesporsel extends StringRequest {
+
+    private static final String REGISTRERINGSURL = "https://folk.ntnu.no/sturlaba/sfa/";
+    private Map<String, String> parametere;   //Brukes av Volley for å sende data til siden
+
+    public RegistrerForesporsel(String brukernavn, String passord, Response.Listener<String> listener) {
+        super(Method.POST, REGISTRERINGSURL, listener, null);
+        parametere = new HashMap<>();
+        parametere.put("epost", brukernavn);
+        parametere.put("passord", passord);
+        parametere.put("registrer", "true");
+    }
+
+    @Override
+    public Map<String, String> getParams(){
+        return parametere;
+    }
+
+}
